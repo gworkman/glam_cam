4a08bf3f7f8458dee3422923ef6367867d306a5f
Gus Workman <gworkman@users.noreply.github.com>
2025-04-04 16:59:26 +0200

    replace home page with liveview

---
+++ lib/glam_cam_web/live/home_live.ex
@@ -0,0 +1,9 @@
+defmodule GlamCamWeb.HomeLive do
+  use GlamCamWeb, :live_view
+
+  def render(assigns) do
+    ~H"""
+    <div>Hello, world!</div>
+    """
+  end
+end
--- lib/glam_cam_web/router.ex
+++ lib/glam_cam_web/router.ex
@@ -17,7 +17,7 @@ defmodule GlamCamWeb.Router do
   scope "/", GlamCamWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", HomeLive
   end
 
   # Other scopes may use custom stacks.
